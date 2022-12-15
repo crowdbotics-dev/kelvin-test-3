d5698f09821b533a8589dded287d64a67ed9f07c
Crowdbotics <engineering@crowdbotics.com>
2022-12-15 19:25:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kelvin-test-3",
+  url: "https://kelvin-test-3.botics.co",
+  api: "https://kelvin-test-3.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
